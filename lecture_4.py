6596787249ff0420dd5e50ef9b18a1bde52cffa8
alexvillr <73649305+alexvillr@users.noreply.github.com>
2022-08-04 17:17:15 +1000

    completed cantor script for cantor line

--- lecture_4.py
+++ lecture_4.py
@@ -4,7 +4,7 @@ Script for generating cantor set
 import numpy as np
 import matplotlib.pylab as plt
 
-levels = 5
+depth = 40
 unit_interval = [0,1]
 num_points = 4
 
@@ -14,6 +14,9 @@ def divide(interval, level=0):
     '''
     plt.plot(interval, [level, level])
     segments = np.linspace(interval[0], interval[1], num_points)
+    if (level < depth):
+        divide(segments[:2], level + 1)
+        divide(segments[2:], level + 1)
 
 divide(unit_interval)
 plt.show()
