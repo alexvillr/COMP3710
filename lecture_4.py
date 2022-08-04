4c259545abf5554b6972945a624aa9649be5728e
alexvillr <73649305+alexvillr@users.noreply.github.com>
2022-08-04 17:09:42 +1000

    Initial script for cantor set for unit interval. as well as work on fractal lab

---
+++ lecture_4.py
@@ -0,0 +1,19 @@
+'''
+Script for generating cantor set
+'''
+import numpy as np
+import matplotlib.pylab as plt
+
+levels = 5
+unit_interval = [0,1]
+num_points = 4
+
+def divide(interval, level=0):
+    '''
+    Divide interval into three parts
+    '''
+    plt.plot(interval, [level, level])
+    segments = np.linspace(interval[0], interval[1], num_points)
+
+divide(unit_interval)
+plt.show
