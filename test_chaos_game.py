5f1f6ff278fe3169a63dc0a12ef2284bae1a150f
alexvillr <73649305+alexvillr@users.noreply.github.com>
2022-08-09 15:10:51 +1000

    finishing up lecture work and doing some extra on assignment

--- test_chaos_game.py
+++ test_chaos_game.py
@@ -1,10 +1,11 @@
 """
 script for playing the chaos game
 """
+from xmlrpc.client import MAXINT
 import numpy as np
 import matplotlib.pylab as plt
 
-n = 3
+n = 4
 
 #generate n points on a unit circle
 r = np.arange(0, n)
@@ -12,15 +13,15 @@ points = np.exp(2.0 * np.pi * 1j * r / n)
 print(points)
 
 #plot points
-res = 100
+res = 1000
 w = np.arange(0, res)
 circle_points = np.exp(2.0 * np.pi * 1j * w / res)
 
 #starting point
 start = 0.1 + 0.5j
 
-plt.plot(np.real(circle_points), np.imag(circle_points), "b-")
-plt.plot(np.real(points), np.imag(points), "r.")
+# plt.plot(np.real(circle_points), np.imag(circle_points), "b-")
+# plt.plot(np.real(points), np.imag(points), "r.")
 
 #play the game
 select = np.random.randint(0, n)
@@ -33,11 +34,13 @@ new_point += start
 #plot it
 
 #full algorithm
-def compute(startloc):
+def compute(startloc, last_point=points[-1]):
     '''
     compute new position for game
     '''
     randloc = np.random.randint(0, n)
+    while (points[randloc] == last_point):
+        randloc = np.random.randint(0, n)
     new_point = points[randloc] - startloc
     new_point = startloc + new_point / 2
     return new_point, points[randloc]
@@ -47,11 +50,11 @@ pl, rloc = compute(start)
 # plt.plot(np.real(circle_points), np.imag(circle_points), "b-")
 # plt.plot(np.real(points), np.imag(points), "r.")
 # plt.plot(np.real(start), np.imag(start), "g.")
-iterations = 1000
+iterations = 100000
 
 next_point = start
 for i in range(iterations):
-    next_point, rloc = compute(next_point)
+    next_point, rloc = compute(next_point, rloc)
     plt.plot(np.real(next_point), np.imag(next_point), "b.")
 
 
