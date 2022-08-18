e1bebdbd89268de257addbf51c63a7cb4fa697e7
alexvillr <73649305+alexvillr@users.noreply.github.com>
2022-08-18 18:11:53 +1000

    assignment finished

--- lorenz.py
+++ lorenz.py
@@ -3,123 +3,35 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 # https://doi.org/10.1016/0375-9601(83)90052-X
 
+# using the sigma, rho and beta values that make the lorenz attractor a fractal
+# according to wikipedia
 def lorenz(x, y, z, sigma=16.0, rho=40.0, beta=4.0):
+    # lorenz equations
     x_dot = sigma * (y - x)
     y_dot = rho * x - y - x * z
     z_dot = x * y - beta * z
 
     return x_dot, y_dot, z_dot
 
-dt = 0.01
-num_steps = 40
-X, Y, Z = np.arange(0.0, num_steps + 1.0, 0.01), np.arange(0.0, num_steps + 1.0, 0.01), np.arange(0.0, num_steps + 1.0, 0.01)
+# setting dt and number of steps for appropriate clarity
+dt=0.01
+num_steps = 20000
 
-xs = tf.constant(X.astype(np.float32))
-ys = tf.constant(Y.astype(np.float32))
-zs = tf.constant(Z.astype(np.float32))
+# initialise empty array and initial values for a clean spiral
+xs, ys, zs = np.empty(num_steps + 1), np.empty(num_steps + 1), np.empty(num_steps + 1)
+xs[0], ys[0], zs[0] = 0.0, 1.0, 1.05
 
+# itterate over array calculating what the new value should be in each case based on previous values
 for i in range(num_steps):
     x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
-    Is = xs + (x_dot * dt)
-    Js = ys + (y_dot * dt)
-    Ks = zs + (z_dot * dt)
+    xs[i+1] = xs[i] + x_dot * dt
+    ys[i+1] = ys[i] + y_dot * dt
+    zs[i+1] = zs[i] + z_dot * dt
 
-xs = Is.numpy()
-ys = Js.numpy()
-zs = Ks.numpy()
 
+# do a 3d projection of the plot making it pretty with some colour choices
 fig = plt.subplot(projection="3d")
 fig.plot(xs, ys, zs, lw=0.5, color="navy")
 fig.scatter(xs, ys, zs, lw=0.1, alpha=0.1, color="red")
 
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# X = np.arange(1.0, 40.0, 0.01)
-# xs = tf.constant(X.astype(np.int32))
-# xs = tf.Variable([0.0, 1.0, 1.05])
-# ns = tf.Variable(tf.zeros_like(xs, tf.float32))
-
-# def lorenz(state, sigma=16.0, rho=40.0, beta=4.0, dt=0.01):
-#     x, y, z = state[0], state[1], state[2]#deconstruct current state
-
-#     #lorenz equation
-#     x_dot = sigma*(y - x)
-#     y_dot = rho*x - y - x*z
-#     z_dot = x*y - beta*z
-
-#     #use derivatives to find next state
-#     newx = (x + x_dot) * dt
-#     newy = (y + y_dot) * dt
-#     newz = (z + z_dot) * dt
-
-#     return [newx, newy, newz]
-
-# for i in range(40):
-#     #compute next state
-#     xs_ = lorenz(xs)
-
-#     xs.assign(xs_)
-    
-
-# plt.imshow(ns.numpy())
-# plt.tight_layout()
-# plt.show()
-
-
-# fig = plt.subplot(projection="3d")
-# fig.plot(states[:, 0], states[:, 1], states[:, 2], lw=0.5, color="navy")
-# fig.scatter(states[:, 0], states[:, 1], states[:, 2], lw=0.1, alpha=0.1, color="red")
-
-# plt.show()
-
-# dt = 0.01
-
-# K = np.arange(0.0, 400.0, 0.01)
-# ks = tf.constant(K.astype(np.float32))
-
-# xs = tf.Variable(ks)
-# ys = tf.Variable(ks)
-# zs = tf.Variable(ks)
-
-# for i in range(200):
-
-#     x_dot, y_dot, z_dot = lorenz(xs[i], ys[i], zs[i])
-#     xs_ = xs + (x_dot * dt)
-#     ys_ = ys + (y_dot * dt)
-#     zs_ = zs + (z_dot * dt)
-
-#     xs.assign(xs_)
-#     ys.assign(ys_)
-#     zs.assign(zs_)
-
-# xs = xs.numpy()
-# ys = ys.numpy()
-# zs = zs.numpy()
-
-# fig = plt.subplot(projection="3d")
-# fig.plot(xs, ys, zs, lw=0.5, color="navy")
-# fig.scatter(xs, ys, zs, lw=0.1, alpha=0.1, color="red")
-
-# plt.show()
