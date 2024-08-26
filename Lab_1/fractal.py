bb522d7eb57e0a335b9598e37187201d8cb6f403
Alex Mac <a.viller@uq.edu.au>
2024-08-26 18:50:33 +1000

    fix import order and style of code

--- Lab_1/fractal.py
+++ Lab_1/fractal.py
@@ -2,17 +2,22 @@ import tensorflow as tf
 import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
+
 print(f"TF Version: {tf.__version__}")
 
-#base layout
+# base layout
 Y, X = np.mgrid[-1.3:1.3:0.0005, -2:1:0.0005]
-Z = X+1j*Y
-#for different zoom
-def zoom (Z, offset, yoffset, zoom):
-    '''
-    takes in how much you want to offset the fractal to the left, and then how many times bigger you want to make the fractal
-    '''
-    return ((Z - offset + 1j * yoffset) * 1/zoom)
+Z = X + 1j * Y
+
+
+# for different zoom
+def zoom(Z, offset, yoffset, zoom):
+    """
+    takes in how much you want to offset the fractal to the left, and then how
+    many times bigger you want to make the fractal
+    """
+    return (Z - offset + 1j * yoffset) * 1 / zoom
+
 
 # Z = zoom(Z, 8, 5.5)
 
@@ -20,10 +25,10 @@ xs = tf.constant(Z.astype(np.complex64))
 zs = tf.Variable(xs)
 ns = tf.Variable(tf.zeros_like(xs, tf.float32))
 
-#mandelbrot
+# mandelbrot
 for i in range(200):
-    #Compute the new values of z: z^2 + x
-    zs_ = zs*zs + xs
+    # Compute the new values of z: z^2 + x
+    zs_ = zs * zs + xs
     # Have we diverged with this new value?
     not_diverged = tf.abs(zs_) < 4
     # Update variables to compute
@@ -31,21 +36,27 @@ for i in range(200):
     zs.assign(zs_)
 
 
-fig = plt.figure(figsize=(16,10))
+fig = plt.figure(figsize=(16, 10))
+
+
 def processFractal(a):
     """Display an array of iteration counts as a
     colorful picture of a fractal."""
-    a_cyclic = (6.28*a/20.0).reshape(list(a.shape)+[1])
-    img = np.concatenate([10+20*np.cos(a_cyclic),
-    30+50*np.sin(a_cyclic),
-    155-80*np.cos(a_cyclic)], 2)
-    img[a==a.max()] = 0
+    a_cyclic = (6.28 * a / 20.0).reshape(list(a.shape) + [1])
+    img = np.concatenate(
+        [
+            10 + 20 * np.cos(a_cyclic),
+            30 + 50 * np.sin(a_cyclic),
+            155 - 80 * np.cos(a_cyclic),
+        ],
+        2,
+    )
+    img[a == a.max()] = 0
     a = img
     a = np.uint8(np.clip(a, 0, 255))
     return a
-    
+
+
 plt.imshow(processFractal(ns.numpy()))
 plt.tight_layout(pad=0)
 plt.show()
-
-
--- Lab_2/test.py
+++ Lab_2/test.py
@@ -1,47 +1,63 @@
+"""
+test file to see if running file works
+"""
+import random
+
+import matplotlib.pyplot as plt
 import torch
 import torchvision
-import torchvision.datasets as datasets
 import torchvision.transforms as tf
-import random
-import matplotlib.pyplot as plt
+from torchvision import datasets
 
 # seed = 999 # for same results
-seed = random.randint(1, 10000) # for variation
+seed = random.randint(1, 10000)  # for variation
 print("Random Seed: ", seed)
 random.seed(seed)
 torch.manual_seed(seed)
 
-img_size = 64
-batch_size = 128
-workers = 4
-root="./data/CelebA"
-nz = 100
+IMG_SIZE = 64
+BATCH_SIZE = 128
+WORKERS = 4
+ROOT = "./data/CelebA"
+NZ = 100
 # Size of feature maps in generator
-ngf = 64
+NGF = 64
 # Size of feature maps in discriminator
-ndf = 64
+NDF = 64
 # Number of training epochs
-num_epochs = 5
+NUM_EPOCHS = 5
 # Learning rate for optimizers
-lr = 2e-4
+LR = 2e-4
 # Beta1 hyperparam for Adam optimizers
-beta1 = 0.5
+BETA1 = 0.5
 
-dataset = datasets.ImageFolder(root=root,
-                           transform=tf.Compose([
-                               tf.Resize(img_size),
-                               tf.CenterCrop(img_size),
-                               tf.ToTensor(),
-                               tf.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
-                           ]))
+dataset = datasets.ImageFolder(
+    root=ROOT,
+    transform=tf.Compose(
+        [
+            tf.Resize(IMG_SIZE),
+            tf.CenterCrop(IMG_SIZE),
+            tf.ToTensor(),
+            tf.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ]
+    ),
+)
 
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, num_workers=workers)
+dataloader = torch.utils.data.DataLoader(
+    dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=WORKERS
+)
 
-DEVICE = 'mps' if torch.backends.mps.is_available() else 'cuda' if torch.cuda.is_available() else 'cpu'
+DEVICE = (
+    "mps"
+    if torch.backends.mps.is_available()
+    else "cuda"
+    if torch.cuda.is_available()
+    else "cpu"
+)
 
 # Plot some training images
 real_batch = next(iter(dataloader))
-plt.figure(figsize=(8,8))
+plt.figure(figsize=(8, 8))
 plt.axis("off")
 plt.title("Training Images")
 grid_img = torchvision.utils.make_grid(real_batch[0])
